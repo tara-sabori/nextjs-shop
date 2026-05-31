71468dd60b62c2635a06f5af184c856f761eacef
tara-sabori <tarasaboori@gmail.com>
2026-06-01 02:52:12 +0330

    add cookies to slug and category route

--- frontend/src/app/(user)/product/(details)/[slug]/page.js
+++ frontend/src/app/(user)/product/(details)/[slug]/page.js
@@ -1,17 +1,61 @@
 import AddToCart from "@/components/AddToCart";
 import api from "@/services/api";
+import {
+  toPersianNumbers,
+  toPersianNumbersWithComma,
+} from "@/utils/toPersianNumbers";
+import { toStringCookies } from "@/utils/toStringCookies";
+import { cookies } from "next/headers";
 import Image from "next/image";
 import { notFound } from "next/navigation";
 
 export default async function ProductBySlugPage({ params }) {
   const { slug } = await params;
-  const { data } = await api.get(`/product/slug/${slug}`);
-  const product = data?.data?.product || null;
+  const cookieStore = await cookies();
+  const strCookies = toStringCookies(cookieStore);
+  // const { data } = await api.get(`/product/slug/${slug}`, {
+  //   headers: {
+  //     Cookie: strCookies,
+  //   },
+  // });
+  // const product = data?.data?.product || null;
+  // if (!product) {
+  //   notFound();
+  // }
+
+  let product = null;
+  try {
+    const response = await api.get(`/product/slug/${slug}`, {
+      headers: {
+        Cookie: strCookies,
+      },
+    });
+    product = response.data?.data?.product || null;
+  } catch (error) {
+    console.error("Error fetching product:", error);
+    if (error.response && error.response.status === 404) {
+      notFound();
+    }
+  }
+
+  if (!product) {
+    notFound(); 
+  }
+
   console.log(product);
   return (
     <div className="container p-5 pt-0 space-y-5 grid grid-cols-3 gap-4">
       {/* information */}
-      <main className="space-y-4 col-span-2">
+      <main className="space-y-4 col-span-3 md:col-span-2 pb-10 md:pb-0">
+        <div className="block md:hidden w-full h-[250px]">
+          <Image
+            src={"/images/not-found.jpg"}
+            width={200}
+            height={200}
+            alt={product?.title}
+            className="w-full h-full rounded-lg"
+          />
+        </div>
         <h2 className="font-semibold text-lg">
           <span className="mygradient">{product?.title}</span>
         </h2>
@@ -25,7 +69,7 @@ export default async function ProductBySlugPage({ params }) {
         </div>
         <div className="flex items-center gap-2">
           <span className="text-secondary-700">موجودی:</span>
-          <span>{product?.countInStock}</span>
+          <span>{toPersianNumbers(product?.countInStock)}</span>
         </div>
         <div className="flex items-center gap-2">
           <span className="text-secondary-700">دسته‌بندی:</span>
@@ -49,8 +93,8 @@ export default async function ProductBySlugPage({ params }) {
           )}
         </div> */}
       </main>
-      <section className="col-span-1 px-10">
-        <div className="w-full h-[250px] mr-auto">
+      <section className="fixed bottom-0 left-0 right-0 shadow-lg md:shadow-none bg-secondary-50 md:bg-white md:static md:col-span-1 px-3 md:px-10">
+        <div className="hidden md:block w-full h-[250px] mr-auto">
           <Image
             src={"/images/not-found.jpg"}
             width={200}
@@ -59,21 +103,21 @@ export default async function ProductBySlugPage({ params }) {
             className="w-full h-full rounded-tr-lg rounded-tl-lg"
           />
         </div>
-        <div className="bg-secondary-50 p-3 shadow-sm">
+        <div className="bg-secondary-50 px-3 py-5 md:p-3 flex flex-row-reverse justify-between md:flex-col md:shadow-sm gap-5">
           <div className="flex items-center gap-2 justify-end">
             <span className="text-secondary-700">قیمت:</span>
             {product?.offPrice ? (
               <div className="flex flex-col">
-                <span className="text-sm font-semibold text-secondary-700">
-                  {product?.offPrice} تومان
+                <span className="text-sm font-semibold text-secondary-700 whitespace-nowrap">
+                  {toPersianNumbersWithComma(product?.offPrice)} تومان
                 </span>
-                <del className="text-xs text-secondary-500">
-                  {product?.price} تومان
+                <del className="text-xs text-secondary-500 whitespace-nowrap">
+                  {toPersianNumbersWithComma(product?.price)} تومان
                 </del>
               </div>
             ) : (
-              <span className="text-sm font-semibold text-secondary-700">
-                {product?.price} تومان
+              <span className="text-sm font-semibold text-secondary-700 whitespace-nowrap">
+                {toPersianNumbersWithComma(product?.price)} تومان
               </span>
             )}
           </div>
--- frontend/src/app/(user)/product/(list)/category/[cat]/page.js
+++ frontend/src/app/(user)/product/(list)/category/[cat]/page.js
@@ -2,16 +2,30 @@
 // import ProductList from "../../_components/product/ProductList";
 import api from "@/services/api";
 import ProductCard from "../../_components/product/ProductCard";
+import { cookies } from "next/headers";
+import { toStringCookies } from "@/utils/toStringCookies";
 
 export default async function ProductByCategoryPage({ params }) {
   const { cat } = await params;
-  const { data } = await api.get(`/product/list?category=${cat}`);
+  const cookieStore = await cookies();
+  const strCookies = toStringCookies(cookieStore);
+  const { data } = await api.get(`/product/list?category=${cat}`, {
+    headers: {
+      Cookie: strCookies,
+    },
+  });
   const { products } = data?.data || [];
   return (
-    <div className="flex items-center gap-4 flex-wrap">
-      {products?.map((product) => (
-        <ProductCard key={product?._id} product={product} />
-      ))}
+    <div className="flex items-stretch justify-center lg:justify-start gap-4 flex-wrap">
+      {products?.length < 1 ? (
+        <div className="w-full py-10 text-center text-secondary-600">
+          محصولی یافت نشد
+        </div>
+      ) : (
+        products?.map((product) => (
+          <ProductCard key={product?._id} product={product} />
+        ))
+      )}
     </div>
   );
 }

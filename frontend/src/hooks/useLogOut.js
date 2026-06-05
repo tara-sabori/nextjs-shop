dd09b6a6ff2fa701c630b347aaa88c3ab487d86e
tara-sabori <tarasaboori@gmail.com>
2026-06-06 00:04:13 +0330

    back to auth page after logout

--- frontend/src/hooks/useLogOut.js
+++ frontend/src/hooks/useLogOut.js
@@ -1,12 +1,15 @@
 import { useAuth } from "@/context/AuthContext";
 import api from "@/services/api";
+import { useRouter } from "next/navigation";
 import toast from "react-hot-toast";
 
 export default function useLogOut() {
   const { dispatch } = useAuth();
+  const router = useRouter();
   const logoutHandler = async () => {
     try {
       const res = await api.post("/user/logout");
+      router.push("/auth");
       dispatch({ type: "logout" });
       toast.success("باموفقیت خارج شدید.");
     } catch (error) {
@@ -14,5 +17,5 @@ export default function useLogOut() {
       toast.error(error?.response?.data?.message || "مشکلی رخ داده است.");
     }
   };
-  return {logoutHandler}
+  return { logoutHandler };
 }

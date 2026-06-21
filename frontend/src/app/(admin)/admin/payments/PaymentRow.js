5cfecb5843ae1661ace15dab04c2030b0dd96905
tara-sabori <tarasaboori@gmail.com>
2026-06-21 22:58:49 +0330

    add paginate to paymentlist

--- frontend/src/app/(admin)/admin/payments/PaymentRow.js
+++ frontend/src/app/(admin)/admin/payments/PaymentRow.js
@@ -1,9 +1,11 @@
+import toLocalDateString from "@/utils/toLocalDateString";
+import { toPersianNumbers, toPersianNumbersWithComma } from "@/utils/toPersianNumbers";
 import React from "react";
 
 const PaymentRow = ({ payment, index }) => {
   return (
-    <tr key={payment?._id}>
-      <td className="table__td">{index + 1}</td>
+    <tr>
+      <td className="table__td">{toPersianNumbers(index + 1)}</td>
       <td className="table__td  whitespace-nowrap truncate">
         {payment?.invoiceNumber}
       </td>
@@ -14,27 +16,30 @@ const PaymentRow = ({ payment, index }) => {
         <div className="flex flex-col gap-y-2">
           <span> {payment?.user?.name}</span>
           <span> {payment?.user?.email}</span>
-          <span className="font-bold">{payment.user.phoneNumber}</span>
+          <span className="font-bold">{payment?.user?.phoneNumber}</span>
         </div>
       </td>
       <td className="table__td">
         <div className="flex flex-col gap-y-2 items-start">
           {payment?.cart?.productDetail.map((product) => {
             return (
-              <span className="badge badge--secondary" key={product?._id}>
+              <span
+                className="badge badge--secondary text-xs"
+                key={product?._id}
+              >
                 {product?.title}
               </span>
             );
           })}
         </div>
       </td>
-      <td className="table__td font-bold text-lg">{payment?.amount}</td>
+      <td className="table__td">{toPersianNumbersWithComma(payment?.amount)}</td>
       <td className="table__td">{toLocalDateString(payment?.createdAt)}</td>
       <td className="table__td">
         {payment?.status === "COMPLETED" ? (
-          <span className="badge badge--success">موفق</span>
+          <span className="badge badge--success text-xs">موفق</span>
         ) : (
-          <span className="badge badge--error">ناموفق</span>
+          <span className="badge badge--error text-xs">ناموفق</span>
         )}
       </td>
     </tr>
--- frontend/src/app/(admin)/admin/payments/PaymentsList.js
+++ frontend/src/app/(admin)/admin/payments/PaymentsList.js
@@ -1,4 +1,6 @@
+import { useSearchParams } from "next/navigation";
 import PaymentRow from "./PaymentRow";
+import Paginate from "@/components/Paginate";
 
 export const adminPaymentListTableTHeads = [
   {
@@ -36,41 +38,53 @@ export const adminPaymentListTableTHeads = [
 ];
 
 const PaymentsList = ({ payments, isLoading }) => {
+  const page = useSearchParams().get("page") || 1;
+  const lastIndex = page * 4;
+  const firstIndex = lastIndex - 4;
+  const records = payments?.slice(firstIndex, lastIndex);
+  const pageCount = Math.ceil(payments?.length / 4);
   return (
-    <div className="overflow-x-auto my-8 shadow-sm">
-      <table className="border-collapse table-auto w-full min-w-200 text-sm">
-        <thead>
-          <tr>
-            {adminPaymentListTableTHeads.map((item) => {
-              return (
-                <th className="whitespace-nowrap table__th" key={item.id}>
-                  {item.label}
-                </th>
-              );
-            })}
-          </tr>
-        </thead>
-        <tbody>
-          {isLoading ? (
+    <>
+      <div className="overflow-x-auto my-8 shadow-sm">
+        <table className="border-collapse table-auto w-full min-w-200 text-sm">
+          <thead>
             <tr>
-              <td colSpan={8} className="text-center p-5">
-                <span>در حال بارگذاری...</span>
-              </td>
+              {adminPaymentListTableTHeads.map((item) => {
+                return (
+                  <th className="whitespace-nowrap table__th" key={item.id}>
+                    {item.label}
+                  </th>
+                );
+              })}
             </tr>
-          ) : payments?.length < 1 ? (
-            <tr>
-              <td colSpan={8} className="text-center p-5">
-                <span>موردی یافت نشد</span>
-              </td>
-            </tr>
-          ) : (
-            payments.map((payment, index) => (
-              <PaymentRow payment={payment} index={index} />
-            ))
-          )}
-        </tbody>
-      </table>
-    </div>
+          </thead>
+          <tbody>
+            {isLoading ? (
+              <tr>
+                <td colSpan={8} className="text-center p-5">
+                  <span>در حال بارگذاری...</span>
+                </td>
+              </tr>
+            ) : records?.length < 1 ? (
+              <tr>
+                <td colSpan={8} className="text-center p-5">
+                  <span>موردی یافت نشد</span>
+                </td>
+              </tr>
+            ) : (
+              records?.map((payment) => (
+                <PaymentRow
+                  key={payment?._id}
+                  payment={payment}
+                  index={payments.indexOf(payment)}
+                />
+              ))
+            )}
+          </tbody>
+        </table>
+      </div>
+      {!isLoading && <Paginate pageCount={pageCount} isClient={true} />}
+    </>
   );
 };
 
